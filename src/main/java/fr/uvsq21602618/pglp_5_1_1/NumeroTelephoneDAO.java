6bebd2f19a733b70ddc165748241beba77c76ef0
nathalie bernard <nathalie.bernard@ens.uvsq.fr>
2020-04-07 17:02:02 +0200

    creation de DAO, PersonnelDAO et NumeroTelephoneDAO

---
+++ src/main/java/fr/uvsq21602618/pglp_5_1_1/NumeroTelephoneDAO.java
@@ -0,0 +1,39 @@
+package fr.uvsq21602618.pglp_5_1_1;
+
+public class NumeroTelephoneDAO extends DAO<NumeroTelephone> {
+    /**
+     * Méthode de création.
+     * @param obj L'objet à créer
+     * @return num
+     */
+    @Override
+    public NumeroTelephone create(NumeroTelephone obj) {
+        NumeroTelephone num = null;
+        return num;
+    }
+    /**
+     * Méthode pour effacer.
+     * @param obj L'objet à effacer
+     * @return num
+     */
+    public void delete(NumeroTelephone obj) {
+        ;
+    }
+    /**
+     * Méthode de mise à jour.
+     * @param obj L'objet à mettre à jour
+     */
+    public NumeroTelephone update(NumeroTelephone obj) { 
+        NumeroTelephone num = null;
+        return num;
+    }
+    /**
+     * Méthode de recherche des informations.
+     * @param id de l'information 
+     * @return num
+     */
+    public NumeroTelephone find(int id) {
+        NumeroTelephone num = null;
+        return num;
+    }
+}
---
+++ src/main/java/fr/uvsq21602618/pglp_5_1_1/PersonnelDAO.java
@@ -0,0 +1,39 @@
+package fr.uvsq21602618.pglp_5_1_1;
+
+public class PersonnelDAO extends DAO<Personnel>{
+    /**
+     * Méthode de création.
+     * @param obj L'objet à créer
+     * @return p
+     */
+    @Override
+    public Personnel create(Personnel obj) {
+        Personnel p = null;
+        return p;
+    }
+    /**
+     * Méthode pour effacer.
+     * @param obj L'objet à effacer
+     * @return p
+     */
+    public void delete(Personnel obj) {
+        ;
+    }
+    /**
+     * Méthode de mise à jour.
+     * @param obj L'objet à mettre à jour
+     */
+    public Personnel update(Personnel obj) { 
+        Personnel p = null;
+        return p;
+    }
+    /**
+     * Méthode de recherche des informations.
+     * @param id de l'information 
+     * @return p
+     */
+    public Personnel find(int id) {
+        Personnel p = null;
+        return p;
+    }
+}
